9b0d7c5b4ad1283854bbe371a1be674bcfd28456
Maksym Kolisnyk <Maksym_Kolisnyk@epam.com>
2016-08-26 19:02:22 +0300

    fix

--- app/pages/kolisnyk-maksym/js/scripts/controller.js
+++ app/pages/kolisnyk-maksym/js/scripts/controller.js
@@ -1,11 +1,8 @@
-(function () {
-    var app = angular.module('store', []);
+var app = angular.module('store', []);
 
-    app.controller("DefaultController", function () {
-        this.message = {
-            first: "Список товаров",
-            second: "Пока будет пусто"
-        }
-    });
-})();
-
+app.controller("DefaultController", function () {
+    this.message = {
+        first: "Список товаров",
+        second: "Пока будет пусто"
+    }
+});
